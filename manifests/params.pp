59a5ce0a4d9dce86cf8bf39bd314bfd58d885760
Allen Canning <allen_canning@putnam.com>
2015-11-06 09:44:59 -0500

    Changed version to 27

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,7 +12,7 @@ class python::params {
   $manage_gunicorn        = true
   $provider               = undef
   $valid_versions = $::osfamily ? {
-    'RedHat' => ['3', '2.7'],
+    'RedHat' => ['3', '27'],
     'Debian' => ['3', '3.3', '2.7'],
     'Suse'   => [],
   }
